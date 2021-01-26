2a6ab11a0a1e846001e042d96a5500e7bbb6b341
Oleg Gribovsky <gribovsky@gmail.com>
2021-01-26 21:01:26 +0300

    Some tests added

--- covid-19Tests/covid_19Tests.swift
+++ covid-19Tests/covid_19Tests.swift
@@ -41,5 +41,18 @@ class covid_19Tests: XCTestCase {
         XCTAssertFalse(result)
     }
     
+    func testThatUserEnterWrongPasswordMoreThan5Times(){
+        var validSum: Int = 0
+        for _ in 1...5 {
+            let result = validator.isLoginCheckPass(loginText: "", passwordText: "")
+            if !result {
+                validSum+=1
+            }
+        }
+        let result = validator.isLoginCheckPass(loginText: "123456", passwordText: "123456")
+       
+        XCTAssertFalse((validSum == 5) && result)
+        
+    }
 
 }
